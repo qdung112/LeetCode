fe3a70c11f6fbf0765f9a60a080b4afff5ce3c5b
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-02 16:28:26 +0700

    remove-element

---
+++ remove-element.java
@@ -0,0 +1,11 @@
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;      
