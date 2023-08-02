ee9aa1db9ef8681c93a7848706b8be00df0ba0a3
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-02 17:52:34 +0700

    Update find-the-index-of-the-first-occurrence-in-a-string

--- find-the-index-of-the-first-occurrence-in-a-string.java
+++ find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,10 +1,11 @@
-if(needle.length() > haystack.length()) return -1;
-        int n = needle.length();
+public int strStr(String haystack, String needle) {
+        if(needle.length() > haystack.length()) return -1;
+        int needlength = needle.length();
         int j = 0;
         boolean check = true;
         String temp = "";
-        for (int i = 0 ; i <= haystack.length() - n ; i ++){
-            temp = haystack.substring(i,i + n);
+        for (int i = 0 ; i <= haystack.length() - needlength ; i ++){
+            temp = haystack.substring(i,i + needlength);
             j = i;
             check = true;
             for(int k = 0; k < n; k++){
@@ -16,3 +17,16 @@ if(needle.length() > haystack.length()) return -1;
             if(check) {return j;}
         }
         return -1;
+    }
+
+public int strStr1(String haystack, String needle) {
+        int haylength = haystack.length();
+        int needlength = needle.length();
+        if(needlength > haylength) return -1;
+        for (int i = 0 ; i <= haylength - needlength ; i ++){
+            if(haystack.substring(i,i + needlength).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
