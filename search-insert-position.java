8c0aa323d21c017f6d73ba7c60177a832204848b
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-03 15:53:20 +0700

    search-insert-position

---
+++ search-insert-position.java
@@ -0,0 +1,10 @@
+public int searchInsert(int[] nums, int target) {
+        int result = nums.length;
+        for(int i = 0 ; i < nums.length ; i ++){
+            if(nums[i] >= target){
+                result = i;
+                break;
+            } 
+        }
+        return result;
+    }
