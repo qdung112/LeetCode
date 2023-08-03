f152f12e37d20330f1c88768de2ad355b26f60f1
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-03 21:15:47 +0700

    add-binary

---
+++ add-binary.java
@@ -0,0 +1,13 @@
+public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while ( i >= 0 || j >= 0 || carry == 1){
+            if(i >= 0) carry += a.charAt(i--) - '0';
+            if(j >= 0) carry += b.charAt(j--) - '0';
+            result.append(carry % 2);
+            carry /= 2;
+        }
+        return result.reverse().toString();   
+}
