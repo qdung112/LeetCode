caab9315caee81020fe5e074d645d3aed090abdf
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-03 19:12:06 +0700

    plus-one

---
+++ plus-one.java
@@ -0,0 +1,22 @@
+public int[] plusOne(int[] digits) {
+        for(int i = digits.length - 1; i >= 0; i --){
+                if(digits[i] < 9){
+                    digits[i] ++;
+                    return digits;
+                } else {
+                        digits[i] = 0;
+                        if( i == 0 ){
+                          return createNewArr(digits);
+                        }
+                }
+            }
+                    return digits;    
+        }
+    public static int[] createNewArr(int[] oldArr){
+        int [] newArr = new int[oldArr.length + 1];
+        for(int j = 1; j < newArr.length; j++ ){
+            newArr[j] = 0;
+        }
+        newArr[0] = 1;
+        return newArr;
+    }
