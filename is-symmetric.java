c37fa52d0c38a7a3988cee2cc08d9ebb332fa39d
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-07 16:30:31 +0700

    Update is-symmetric

--- is-symmetric.java
+++ is-symmetric.java
@@ -1,8 +1,9 @@
  public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
         TreeNode left = root.left;
         TreeNode right = root.right;
         if(left == null && right == null) return true;
-        if(left == null || right == null) return false;
+        if(left == null || right == null || left.val != right.val) return false;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(left);
         queue.offer(right);
@@ -17,4 +18,16 @@
             queue.offer(right.left);
         }
         return true;
+    }
+
+ public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        return isSame(left,right);
+    }
+    public static boolean isSame(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        if(left == null || right == null || left.val != right.val) return false;
+        return isSame(left.left,right.right) && isSame(left.right,right.left);
     }
