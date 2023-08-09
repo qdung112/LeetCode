c0b0b4f745159b2fcb4c6a16019a4aa56889b01a
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-09 21:37:08 +0700

    max-sub-array

---
+++ max-sub-array.java
@@ -0,0 +1,15 @@
+public int maxSubArray(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(currentSum + nums[i], nums[i]);
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+        }
+
+        return maxSum;     
+    }
