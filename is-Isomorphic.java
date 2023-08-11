0649d14828a3011aefb4c3fcd844103139babef3
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-11 18:08:45 +0700

    is-Isomorphic

--- is-Isomorphic.java
+++ is-Isomorphic.java
@@ -19,3 +19,13 @@ public boolean isIsomorphic(String s, String t) {
         }
         return true;
     }
+ public boolean isIsomorphic(String s, String t) {
+     Map<Character, Integer> map1 = new HashMap<>();
+     Map<Character, Integer> map2 = new HashMap<>();
+
+     for(Integer i = 0; i <s.length(); i ++) {
+         if(map1.put(s.charAt(i), i) != map2.put(t.charAt(i), i))
+         return false;
+     }
+     return true;
+    }
