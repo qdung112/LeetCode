e95bdbe0643046fb1ad0d3bb528e7f3742b98f36
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-15 16:35:52 +0700

    find-disappeared-numbers

---
+++ find-disappeared-numbers.java
@@ -0,0 +1,16 @@
+ public List<Integer> findDisappearedNumbers(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i=0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }else i++;
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=i+1) ans.add(i+1);
+        }
+        return ans;
+    }
