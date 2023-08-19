18008a742607a0453c88aa521e55b961b9c77aec
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-19 15:05:33 +0700

    array-strings-are-equal

---
+++ array-strings-are-equal.java
@@ -0,0 +1,26 @@
+ public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        for(String word : word1) {
+            sb1.append(word);
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for(String word : word2) {
+            sb2.append(word);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
+
+public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+         // Creates a new string by combining all the strings in word1.
+        StringBuilder word1Combined = new StringBuilder();
+        for (String s : word1) {
+            word1Combined.append(s);
+        }
+        // Creates a new string by combining all the strings in word2.
+        StringBuilder word2Combined = new StringBuilder();
+        for (String s : word2) {
+            word2Combined.append(s);
+        }
+        // Returns true if both string are the same.
+        return word1Combined.compareTo(word2Combined) == 0;
+    }
