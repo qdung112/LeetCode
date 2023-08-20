d3215ecfb6f3d7347c93ac8d6c5a5917351d4ddd
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-20 15:30:13 +0700

    find-error-nums

---
+++ find-error-nums.java
@@ -0,0 +1,13 @@
+public int[] findErrorNums(int[] nums) {
+        int [] arr = new int[nums.length + 1];
+        int missing = 1;
+        int dup = -1;
+        for(int i = 0; i < nums.length ; i ++){
+            arr[nums[i]] += 1;
+        }
+        for(int i = 1 ; i < arr.length; i ++){
+            if(arr[i] == 0) {missing = i;}
+            if(arr[i] > 1) {dup = i;}
+        }
+        return new int[]{dup,missing};
+    }
