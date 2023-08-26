822ce3b4a39202bf9aa330fefb5d77cd82569487
qdung112 <94347165+qdung112@users.noreply.github.com>
2023-08-26 15:44:19 +0700

    unique-Occurrences

---
+++ unique-Occurrences.java
@@ -0,0 +1,11 @@
+ Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        
+        // Store the frequency count of elements in the unordered set.
+        Set<Integer> freqSet = new HashSet<>(freq.values());
+        
+        // If the set size is equal to the map size, 
+        // It implies frequency counts are unique.
+        return freq.size() == freqSet.size();
